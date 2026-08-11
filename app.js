9e55b5616cf2dda5ac62bdc2e3f20ed97d70d392
mariamatwa-24 <mariamelhady11@gmail.com>
2026-08-11 14:19:28 -0700

    Add admin platform functions and messages in Arabic

---
+++ app.js
@@ -0,0 +1,23 @@
+const adminName = "مديرة ورشة المبرمج الصغير";
+
+function showMessage(message) {
+  alert(message);
+}
+
+function openStudents() {
+  showMessage("قسم إدارة الطلاب جاهز للتوصيل بقاعدة البيانات.");
+}
+
+function openCourses() {
+  showMessage("قسم الكورسات والدروس جاهز.");
+}
+
+function openVideos() {
+  showMessage("قسم الفيديوهات جاهز.");
+}
+
+function openResults() {
+  showMessage("قسم النتائج جاهز.");
+}
+
+console.log(${adminName} - Admin Platform);
